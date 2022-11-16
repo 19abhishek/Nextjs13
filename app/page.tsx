9cb729dd4634c27f52a27ef9d81b4baa18cfdd4f
Abhishek Kumar <abhishek.kumar@gullak.money>
2022-11-16 23:45:29 +0530

    Tailwind css issue fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "../styles/globals.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="text-red-500 bg-slate-500 underline p-10">
+      Helllo
+      <div className="text-blue-500 underline">Hi</div>
+    </div>
+  );
 }
 
 export default Home;
